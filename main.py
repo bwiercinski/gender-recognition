47a02468a1ac6352fefc1c7ee07e6f6c1b69b898
Bartłmoiej Wierciński <bmwbartek1@gmail.com>
2019-04-08 01:37:25 +0200

    first run network

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ from process_image import get_image, process_image
 
 if __name__ == "__main__":
     print("Load data")
-    # with open("data/test_data/t_1000_200.pkl", "rb") as file:
-    #     ((train_labels, train_data), (test_labels, test_data)) = pickle.load(file)
-
-    ((train_labels, train_data), (test_labels, test_data)) = get_test_data(1000, 200)
+    with open("data/test_data/t_1000_200.pkl", "rb") as file:
+        ((train_labels, train_data), (test_labels, test_data)) = pickle.load(file)
+    # ((train_labels, train_data), (test_labels, test_data)) = get_test_data(1000, 200)
 
     print("Create model")
     model = keras.Sequential([
@@ -38,10 +37,10 @@ if __name__ == "__main__":
 
     print("My tests")
     kasias = [
+        process_image(get_image("my_image/" + "Bartek_0001.jpg"), crop=False),
         process_image(get_image("my_image/" + "Kasia_0001.jpg"), crop=False),
         process_image(get_image("my_image/" + "Kasia_0002.jpg"), crop=False)
     ]
     predictions = model.predict(np.array(kasias))
 
-    print("Kasia prediction:", np.argmax(predictions[0]))
-    print("Kasia prediction:", np.argmax(predictions[1]))
+    print("Kasia prediction:", np.argmax(predictions, axis=1))
